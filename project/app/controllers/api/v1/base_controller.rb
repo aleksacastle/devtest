9cf3980498842274375c8fbd991a30fdd6ab027c
aleksacastle <aaklochko@gmail.com>
2018-07-22 22:58:41 +0300

    added base controller for private consumption with token authorisation

---
+++ project/app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,25 @@
+module Api::V1
+  class BaseController < Api::BaseController
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    before_action :authenticate
+
+    private
+
+    def authenticate
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+        User.find_by(token: auth_token)
+      end
+    end
+
+    def render_unauthorized
+      render json: {
+        error_message: "Bad credentials"
+      }, status: :unauthorized
+    end
+  end
+end
